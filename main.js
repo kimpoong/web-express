24944bf7928074c1999e24f537169bcc593e6b89
kimpoong <sjin0704@gmail.com>
2021-11-16 15:58:12 +0900

    add create feature

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 var app = http.createServer((req, res) => {
     var queryData = url.parse(req.url, true).query;
     var pathname = url.parse(req.url, true).pathname;
     
-    if (pathname != '/') {
-        res.writeHead(404);
-        res.end('not found');
-    }
-    else {
+    if (pathname == '/') {
         var title = queryData.id;
         var content;
         var filelist = fs.readdirSync("./data");
@@ -31,12 +28,13 @@ var app = http.createServer((req, res) => {
             <!DOCTYPE html>
             <html>
                 <head>
-                    <title>Web-NodeJs/${title}</title>
+                    <title>Web-NodeJs:${title}</title>
                     <meta charset="utf-8">
                 </head>
                 <body>
                     <h1><a href="/">Web-NodeJs</a></h1>
                     ${list_html}
+                    <p><a href="/create">create</a></p>
                     <h2>${title}</h2>
                     <p>${content}</p>
                 </body>
@@ -44,6 +42,43 @@ var app = http.createServer((req, res) => {
             `
         res.writeHead(200);
         res.end(template);
+    }
+    else if (pathname == '/create') {
+        var template = `
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Web-NodeJs:create</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">Web-NodeJs</a></h1>
+                    <form action="/create_doc" method="post">
+                        <p><input type="text" name="title" placeholder="title"></p>
+                        <p><textarea name="content" placeholder="content"></textarea></p>
+                        <p><input type="submit"></p>
+                    </form>
+                </body>
+            </html>
+            `
+        res.writeHead(200);
+        res.end(template);
+    }
+    else if (pathname == '/create_doc') {
+        var body = '';
+        req.on('data', (data) => {
+            body += data;
+        });
+        req.on('end', () => {
+            var post = qs.parse(body);
+            fs.writeFileSync(`data/${post.title}`, post.content, 'utf8');
+            res.writeHead(302, { Location: '/' });
+            res.end();
+        })
+    }
+    else {
+        res.writeHead(404);
+        res.end('not found');
     }
     
 });
